a5bbe79e1a4ba62230132d59c727dfe25ce7796d
Glorgs <49840094+Glorgs@users.noreply.github.com>
2021-09-16 13:45:24 +0200

    Update env-config.js

--- env-config.js
+++ env-config.js
@@ -2,5 +2,5 @@
 const prod = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  'process.env.BACKEND_URL': prod ? '/Web' : ''
+  'process.env.BACKEND_URL': prod ? 'http://jonathanbaudillon.com/' : ''
 }
