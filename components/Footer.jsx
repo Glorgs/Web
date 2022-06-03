dee8e4a3f45434f8b88d45d9669ffcdaf3243df4
Glorgs <49840094+Glorgs@users.noreply.github.com>
2022-06-03 21:50:42 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,6 +12,11 @@ export default function Footer() {
                 <div className="opacity-50 text-sm tracking-widest">EMAIL</div>
                 <a href="mailto:jonathanbaudillon@yahoo.fr" target="_self" className="mt-5 block opacity-70 tracking-wider hover:underline">jonathanbaudillon@yahoo.fr</a>
             </div>
+
+            <div className="w-full lg:w-auto">
+                <div className="opacity-50 text-sm tracking-widest">ITCH</div>
+                <a href="https://glorgs.itch.io/" target="_self" className="mt-5 block opacity-70 tracking-wider hover:underline">glorgs.itch.io</a>
+            </div>
         </div>
     )
 }
